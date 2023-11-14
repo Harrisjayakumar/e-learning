057c2a3497d4c34a43c9321e4249cb72fedbb201
Harrisjayakumar V <111297556+Harrisjayakumar@users.noreply.github.com>
2023-11-14 17:57:52 +0530

    elearning

---
+++ src/data.js
@@ -0,0 +1,337 @@
+// import images
+
+import LogoImg from '../src/assets/img/header/logo.png';
+import HeroImg from '../src/assets/img/hero/lerning.svg';
+import OverviewProductImg from '../src/assets/img/overview/overvier.jpg';
+import FacebookImg from '../src/assets/img/overview/brands/facebook.svg';
+import GoogleImg from '../src/assets/img/overview/brands/google.svg';
+import CocaColaImg from '../src/assets/img/overview/brands/coca-cola.svg';
+import LinkedInImg from '../src/assets/img/overview/brands/linkedin.svg';
+import SamsungImg from '../src/assets/img/overview/brands/samsung.svg';
+import Feature1Img from '../src/assets/img/features/take.svg';
+import Feature2Img from '../src/assets/img/features/feature2-img.svg';
+import Feature3Img from '../src/assets/img/features/feature3-img.svg';
+import ArrowRightImg from '../src/assets/img/features/arrow-right.svg';
+import CardIconImg1 from '../src/assets/img/product/cards/icon1.svg';
+import CardIconImg2 from '../src/assets/img/product/cards/icon2.svg';
+import CardIconImg3 from '../src/assets/img/product/cards/icon3.svg';
+import PricingIcon1 from '../src/assets/img/pricing/icon1.svg';
+import PricingIcon2 from '../src/assets/img/pricing/icon2.svg';
+import PricingIcon3 from '../src/assets/img/pricing/icon3.svg';
+import AvatarImg1 from '../src/assets/img/testimonial/avatar1.png';
+import AvatarImg2 from '../src/assets/img/testimonial/avatar2.png';
+import AvatarImg3 from '../src/assets/img/testimonial/avatar3.png';
+import AvatarImg4 from '../src/assets/img/testimonial/avatar4.png';
+import AvatarImg5 from '../src/assets/img/testimonial/avatar5.png';
+import CtaImg1 from '../src/assets/img/cta/cta.svg';
+import CtaImg2 from '../src/assets/img/cta/dolar.svg';
+
+import FacebookIcon from '../src/assets/img/copyright/facebook.svg';
+import TwitterIcon from '../src/assets/img/copyright/twitter.svg';
+import LinkedinIcon from '../src/assets/img/copyright/linkedin.svg';
+
+import ReactJs from '../src/assets/img/product/cards/reactjs.jpg'
+
+import Graphql from '../src/assets/img/product/cards/Graphql.jpg'
+
+import Django from '../src/assets/img/product/cards/django.jpg'
+import html5 from '../src/assets/img/product/cards/html5.jpg'
+
+export const header = {
+  logo: LogoImg,
+  btnText: 'Request a demo',
+};
+
+export const nav = [
+  { name: 'courses', href: '/' },
+  { name: 'About us', href: '/' },
+  { name: 'Cart', href: '/' },
+  { name: 'Feedback', href: '/' },
+];
+
+export const hero = {
+  title: 'Find The Best Courses To get Statrted ',
+  subtitle: 'Learn and Start Your Life ',
+  btnText: 'More',
+  compText: '— programming, marketing ,business',
+  image: HeroImg,
+};
+
+export const overview = {
+  productImg: OverviewProductImg,
+  brands: [
+    {
+      image: FacebookImg,
+      delay: 300,
+    },
+    {
+      image: GoogleImg,
+      delay: 400,
+    },
+    {
+      image: CocaColaImg,
+      delay: 500,
+    },
+    {
+      image: LinkedInImg,
+      delay: 600,
+    },
+    {
+      image: SamsungImg,
+      delay: 700,
+    },
+  ],
+};
+
+export const features = {
+  feature1: {
+    pretitle: 'Alwalys online',
+    title: 'Learn Anything and Get Certificates',
+    subtitle:
+      'Tellus lacus morbi sagittis lacus in. Amet nisl at mauris enim accumsan nisi, tincidunt vel. Enim ipsum, amet quis ullamcorper eget ut.',
+    btnLink: 'Learn more',
+    btnIcon: ArrowRightImg,
+    image: Feature1Img,
+  },
+  feature2: {
+    pretitle: 'Free some cost',
+    title: 'save the courses anytime when you need',
+    subtitle:
+      'Tellus lacus morbi sagittis lacus in. Amet nisl at mauris enim accumsan nisi, tincidunt vel. Enim ipsum, amet quis ullamcorper eget ut.',
+    btnLink: 'Learn more',
+    btnIcon: ArrowRightImg,
+    image: Feature2Img,
+  },
+  feature3: {
+    pretitle: 'Use anytime',
+    title: 'Use anytime when you need',
+    subtitle:
+      'Tellus lacus morbi sagittis lacus in. Amet nisl at mauris enim accumsan nisi, tincidunt vel. Enim ipsum, amet quis ullamcorper eget ut.',
+    btnLink: 'Learn more',
+    btnIcon: ArrowRightImg,
+    image: Feature3Img,
+  },
+};
+
+export const product = {
+  title: 'Top Rated Courses.',
+  subtitle:
+    'Tellus lacus morbi sagittis lacus in. Amet nisl at mauris enim aumsan nisi, tincidunt vel. Enim ipsum, at quis ullamcorper eget ut.',
+  cards: [
+    {
+      icon: Graphql,
+      title: 'GraphQl Full Courses',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 200,
+    },
+    {
+      icon: ReactJs,
+      title: 'React Js Courses',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 400,
+    },
+    {
+      icon: Django,
+      title: 'Django Courses',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 400,
+    },
+    {
+      icon: html5,
+      title: 'HTML5 Courses',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 400,
+    },
+    {
+      icon: ReactJs,
+      title: 'React Js Courses',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 400,
+    },
+    
+  ],
+};
+
+
+
+
+
+
+export const products = {
+  title: 'The Product we work with.',
+  subtitle:
+    'Tellus lacus morbi sagittis lacus in. Amet nisl at mauris enim aumsan nisi, tincidunt vel. Enim ipsum, at quis ullamcorper eget ut.',
+  cardss: [
+    {
+      icon: CardIconImg1,
+      title: 'Learn Anything From Anywhere ',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 200,
+    },
+    {
+      icon: CardIconImg2,
+      title: 'Purchase Now',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 400,
+    },
+    {
+      icon: CardIconImg3,
+      title: 'Save your Courses',
+      subtitle: 'Elit esse cillum dolore eu fugiat nulla pariatur',
+      delay: 600,
+    },
+  ],
+};
+
+
+
+
+
+
+
+
+
+
+
+export const pricing = {
+  title: 'Choose your flexible plan.',
+  cards: [
+    {
+      icon:CardIconImg2 ,
+      title: '  React Js Cour',
+      services: [
+        { name: 'Store unlimited data' },
+        { name: 'Export to pdf, xls, csv' },
+        { name: 'Cloud server support' },
+      ],
+      price: '$9.99',
+      userAmount: 'up to 3 user + 1.99 per user',
+      btnText: 'Get this',
+      delay: 300,
+    },
+    {
+      icon: PricingIcon2,
+      title: 'Silver Plan',
+      services: [
+        { name: 'Store unlimited data' },
+        { name: 'Export to pdf, xls, csv' },
+        { name: 'Cloud server support' },
+      ],
+      price: '$19.99',
+      userAmount: 'up to 3 user + 1.99 per user',
+      btnText: 'Get this',
+      delay: 600,
+    },
+    {
+      icon: PricingIcon3,
+      title: 'Diamond Plan',
+      services: [
+        { name: 'Store unlimited data' },
+        { name: 'Export to pdf, xls, csv' },
+        { name: 'Cloud server support' },
+      ],
+      price: '$29.99',
+      userAmount: 'up to 3 user + 1.99 per user',
+      btnText: 'Get this',
+      delay: 900,
+    },
+  ],
+};
+
+export const testimonials = {
+  title: 'We have millions of best wishers',
+  clients: [
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg1,
+      name: 'Reaxt',
+      position: 'CEO',
+      borderColor: '#FF7235',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg2,
+      name: 'Shirley Hand',
+      position: 'CEO',
+      borderColor: '#FFBE21',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg3,
+      name: 'Dr. Olivia Hansen',
+      position: 'CEO',
+      borderColor: '#4756DF',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg4,
+      name: 'Aubrey Sanford',
+      position: 'CEO',
+      borderColor: '#3EC1F3',
+    },
+    {
+      message:
+        'Eleifend fames amet, fames enim. Ullamcorper pellentesque ac volutpat nibh aliquet et, ut netus. Vel, fringilla sit eros pretium',
+      image: AvatarImg5,
+      name: 'Terri Conroy',
+      position: 'CEO',
+      borderColor: '#BB7259',
+    },
+  ],
+};
+
+export const cta = {
+  title: '5M+ People from  different countries',
+  subtitle: 'Take Action and find the est courses for you ',
+  btnText: 'Take course',
+  img1: CtaImg1,
+  img2: CtaImg2,
+};
+
+export const footer = {
+  logo: LogoImg,
+  links: [
+    { name: 'Home', href: '/' },
+    { name: 'courses', href: '/' },
+    { name: 'about us', href: '/' },
+    { name: 'cart', href: '/' },
+    { name: 'Feadback', href: '/' },
+    
+  ],
+  legal: [
+    { name: 'Terms of use', href: '/' },
+    { name: 'Terms of conditions', href: '/' },
+    { name: 'Privacy policy', href: '/' },
+    { name: 'Cookie policy', href: '/' },
+  ],
+  newsletter: {
+    title: 'Newsletter',
+    subtitle: 'Over 25000 people have subscribed',
+  },
+  form: {
+    placeholder: 'Enter your email',
+    btnText: 'Subscribe',
+    smallText: 'We don’t sell your email and spam',
+  },
+};
+
+export const copyright = {
+  link1: {
+    name: 'Privacy & Terms',
+    href: '/',
+  },
+  link2: {
+    name: 'Contact us',
+    href: '/',
+  },
+  copyText: 'Copyright @ 2023 zikoulearn',
+  social: [
+    { icon: FacebookIcon, href: '/' },
+    { icon: TwitterIcon, href: '/' },
+    { icon: LinkedinIcon, href: '/' },
+  ],
+};
